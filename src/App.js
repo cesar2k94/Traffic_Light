371bc04cccf5deb1860cfb7028eaa6ecc81ac21a
Julio <juliomederosarias@gmail.com>
2021-03-05 23:10:57 -0300

    version 0.2

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+
+  const [color, setColor] = useState(["redOpaco", "orangeOpaco", "greenOpaco"]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="total">
+      <div className="inicio">
+      </div >
+      <div className="body">
+        <div className="color">
+          <button className={color[0]} onClick={() => setColor(["red", "orangeOpaco", "greenOpaco"])}></button>
+          <button className={color[1]} onClick={() => setColor(["redOpaco", "orange", "greenOpaco"])}></button>
+          <button className={color[2]} onClick={() => setColor(["redOpaco", "orangeOpaco", "green"])}></button>
+        </div>
+      </div>
     </div>
   );
 }
